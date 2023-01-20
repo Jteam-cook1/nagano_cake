7d900220bab39eb347d12649291847554591b5bd
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-01-20 08:21:55 +0000

    管理者側の会員情報ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,30 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customers_path
+    else
+      flash[:customer_updated_error] = "会員情報が正常に保存されませんでした。"
+      redirect_to edit_admin_customer_path(@customer)
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email, :is_active)
   end
 end
