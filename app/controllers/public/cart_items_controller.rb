6f7cade2d4c8e71cd19875936d45c726c4ec1bf3
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-01-22 10:23:48 +0000

    kashiwa5

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,7 @@
 class Public::CartItemsController < ApplicationController
-  
+
   before_action :authenticate_customer!
-  
+
   def index
     @cart_items = current_customer.cart_items
     @cart_item_total_price = 0
@@ -17,7 +17,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = current_customer.cart_item.new(cart_item_params)
+    @cart_item = current_customer.cart_items.new(cart_item_params)
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
        cart_item = current_public.cart_items.find_by(item_id: params[:cart_item][:item_id])
        cart_item.amount += params[:cart_item][:amount].to_i
