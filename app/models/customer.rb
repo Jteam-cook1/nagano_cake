39acdce555a771128c3eb144db3f27238570b98e
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-01-23 05:25:10 +0000

    バリデーションエラー解決

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   validates :last_name,  presence: true
   validates :first_name, presence: true
   validates :last_name_kana,  presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
@@ -19,10 +20,20 @@ class Customer < ApplicationRecord
   # 郵便番号のフォーマット指定 ハイフン無し７桁固定 Viewのフォーム設定
   validates :address, presence: true
   validates :email,  presence: true
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password_confirmation, presence: true, length: { minimum: 6 }, on: :create
 
 
+  # with_options on: :step1 do
+  #   validates :last_name,  presence: true
+  #   validates :first_name, presence: true
+  #   validates :last_name_kana,  presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+  #   validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+  #   validates :phone_number, presence: true, format: {with: /\A\d{10}$|^\d{11}\z/, message: '10桁か11桁の電話番号を入力してください。'}
+  #   validates :postcode,  presence: true, format: {with: /\A\d{7}\z/}
+  #   validates :address, presence: true
+  #   validates :email,  presence: true
+  # end
 
   def full_name
     self.last_name + " " + self.first_name
