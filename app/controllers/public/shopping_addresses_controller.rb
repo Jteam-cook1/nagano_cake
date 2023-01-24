cab2b45b5b8d85b70a949137f936d08adad30f2e
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-01-24 08:06:15 +0000

    配送先登録/一覧

--- app/controllers/public/shopping_addresses_controller.rb
+++ app/controllers/public/shopping_addresses_controller.rb
@@ -6,14 +6,32 @@ class Public::ShoppingAddressesController < ApplicationController
   end
 
   def edit
+    @shopping_address = ShoppingAddress.find(params[:id])
   end
 
   def create
+    @shopping_address = ShoppingAddress.new(shopping_address_params)
+    @shopping_address.customer_id = current_customer.id
+    @shopping_address.save
+
+    redirect_to shopping_addresses_path
   end
 
   def update
+    shopping_address = ShoppingAddress.find(params[:id])
+    shopping_address.update(shopping_address_params)
+    redirect_to shopping_addresses_path
   end
 
   def destroy
+    @shopping_address = ShoppingAddress.find(params[:id])
+    @shopping_address.destroy
+    redirect_to shopping_addresses_path
+  end
+
+  private
+
+  def shopping_address_params
+    params.require(:shopping_address).permit(:postcode, :address, :name)
   end
 end
