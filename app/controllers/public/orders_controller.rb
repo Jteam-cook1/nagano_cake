410fe04aa2ca8788576cf838bf3b0119bde3bcc9
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-01-24 03:39:27 +0000

    合計金額

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -63,8 +63,14 @@ class Public::OrdersController < ApplicationController
     @order_details = @order.order_details
     @sub_total = 0
     @total_price = 0
+    @total = 0
     @order.shipping_fee = 800
-    
+     @order_details.each do |order_detail|
+       @total = @total += order_detail.subtotal
+       @total_price += order_detail.subtotal
+     end
+
+
   end
 
   private
