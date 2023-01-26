3f9bf24dd4297b25f3b12680cc1adafcb2c55626
EC2 Default User <ec2-user@ip-172-31-47-105.ap-northeast-1.compute.internal>
2023-01-26 03:36:41 +0000

    watanabe

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,13 +2,13 @@ class Public::ItemsController < ApplicationController
    before_action :authenticate_customer!, except: [:index,:show, :search]
 
   def index
-
+    @items = Item.where(is_active: true)
     @genres = Genre.all
     if params[:genre_id]
-    @items = Item.where(genre_id: params[:genre_id]).page(params[:page])
+    @items = @items.where(genre_id: params[:genre_id]).page(params[:page])
     @title = Genre.find(params[:genre_id]).name
     else
-    @items = Item.page(params[:page]).per(8)
+    @items = @items.page(params[:page]).per(8)
     @title = "商品"
     end
     #@item = Item.find(params[:id])
